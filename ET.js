a50dbc7aa1a0277478c7f312c625efe3f2b992a1
AbhayBarolia <abhay28barolia@gmail.com>
2023-03-19 04:06:58 +0530

    axios3

--- ET.js
+++ ET.js
@@ -2,38 +2,18 @@ let list=document.getElementById("list");
 let count=1000;
 function submitform(event){
     event.preventDefault();
-    let li=document.createElement("li");
-    let str=count+"      "+document.getElementById('amount').value+"       "+document.getElementById('desc').value+"       "+document.getElementById('category').value+"      ";
-    li.appendChild(document.createTextNode(str));
-    let btn=document.createElement("Delete");
-    btn.className="delete";
-    btn.setAttribute("type","button");
-    btn.style.border= "thick solid #0000FF";
-    btn.appendChild(document.createTextNode("Delete")); 
-    li.appendChild(btn);
-    list.appendChild(li);
-    let edbtn=document.createElement("edit");
-    edbtn.className="edit";
-    edbtn.setAttribute("type","button");
-    edbtn.appendChild(document.createTextNode("Edit")); 
-    edbtn.style.border= "thick solid #0000FF";
-    li.appendChild(edbtn);
-    edbtn.style.margin="5px";
-   
-
-    
-
-let obj= {
+    let obj= {
     amount:document.getElementById('amount').value,
     desc:document.getElementById('desc').value,
     category:document.getElementById('category').value,    
-};
+    };
 
-let i=count;
-let ls=JSON.stringify(obj);
-axios.post("https://crudcrud.com/api/a67dbf06587f4775bda035004815effe/ExpenseTracker",obj).then((res)=>{console.log(res);}).catch((err)=>{console.log(err);});
-localStorage.setItem(i,ls);
-count++;
+    axios.post("https://crudcrud.com/api/a67dbf06587f4775bda035004815effe/ExpenseTracker",obj).then((res)=>{console.log(res);
+    let dt= res.data;
+    const str =`${dt.amount} ${dt.category} ${dt.desc} ${dt._id}`          
+    showData(str);
+    }).catch((err)=>{console.log(err);});
+
 }
 list.addEventListener("click",removeItem);
 list.addEventListener("click",editItem);
@@ -46,14 +26,35 @@ function removeItem(e)
         {
             let li=e.target.parentElement;
             let str=li.textContent;
-            let str1=str.toString().substring(0,4);
-            localStorage.removeItem(Number(str1));
+            let str1=str.slice(-34);
+            let str2=str1.substring(0,24);
+            
             list.removeChild(li);
-            axios.delete("https://crudcrud.com/api/a67dbf06587f4775bda035004815effe/ExpenseTracker/641621a222534003e8c8e5d8").then((res)=>{console.log(res);}).catch((err)=>{console.log(err);});
+            axios.delete("https://crudcrud.com/api/a67dbf06587f4775bda035004815effe/ExpenseTracker/"+str2).then((res)=>{console.log(res);}).catch((err)=>{console.log(err);});
             
         }
 
     }
+    }
+
+    window.addEventListener("DOMContentLoaded",domLoaded);
+    function domLoaded(e)
+    {
+        axios.get("https://crudcrud.com/api/a67dbf06587f4775bda035004815effe/ExpenseTracker").then((res)=>{
+           
+          
+            for(let i=0; i<res.data.length;i++)
+            {
+                let dt= res.data[i];
+                const str =`${dt.amount} ${dt.category} ${dt.desc} ${dt._id}`
+                
+               showData(str);
+            }
+        
+          
+    })
+    .catch((err)=>{console.log(err);});
+        
     }
 
     function editItem(e)
@@ -71,4 +72,25 @@ function removeItem(e)
             
         }
     }    
+
+    function showData(str)
+    {
+        
+        let li=document.createElement("li");
+        li.appendChild(document.createTextNode(str));
+        let btn=document.createElement("Delete");
+        btn.className="delete";
+        btn.setAttribute("type","button");
+        btn.style.border= "thick solid #0000FF";
+        btn.appendChild(document.createTextNode("Delete")); 
+        li.appendChild(btn);
+        list.appendChild(li);
+        let edbtn=document.createElement("edit");
+        edbtn.className="edit";
+        edbtn.setAttribute("type","button");
+        edbtn.appendChild(document.createTextNode("Edit")); 
+        edbtn.style.border= "thick solid #0000FF";
+        li.appendChild(edbtn);
+        edbtn.style.margin="5px";
+    }
 
