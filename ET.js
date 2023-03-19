48f6bc07ab26b9f46a0b6e1fb82e03ea9b681d8c
AbhayBarolia <abhay28barolia@gmail.com>
2023-03-19 05:36:14 +0530

    GET, PUT,POST,DELETE Dynamic

--- ET.js
+++ ET.js
@@ -1,19 +1,37 @@
+let editFlag =false;
+let edUrl="";
 let list=document.getElementById("list");
-let count=1000;
 function submitform(event){
+   
     event.preventDefault();
     let obj= {
     amount:document.getElementById('amount').value,
     desc:document.getElementById('desc').value,
     category:document.getElementById('category').value,    
     };
-
+    if(editFlag===false){
     axios.post("https://crudcrud.com/api/a67dbf06587f4775bda035004815effe/ExpenseTracker",obj).then((res)=>{console.log(res);
     let dt= res.data;
     const str =`${dt.amount} ${dt.category} ${dt.desc} ${dt._id}`          
     showData(str);
     }).catch((err)=>{console.log(err);});
 
+    }
+    else
+    {
+        axios.patch("https://crudcrud.com/api/a67dbf06587f4775bda035004815effe/ExpenseTracker/"+edUrl,obj)
+        .then((res)=>{console.log(res);})
+        .catch((err)=>{console.log(err);});
+        
+        axios.get("https://crudcrud.com/api/a67dbf06587f4775bda035004815effe/ExpenseTracker"+edUrl).then((res)=>{console.log(res);
+        let dt= res.data;
+        const str =`${dt.amount} ${dt.category} ${dt.desc} ${dt._id}`          
+        showData(str);
+        }).catch((err)=>{console.log(err);});
+
+        editFlag=false;
+        edUrl="";
+    }
 }
 list.addEventListener("click",removeItem);
 list.addEventListener("click",editItem);
@@ -60,16 +78,24 @@ function removeItem(e)
     function editItem(e)
     {
         if(e.target.classList.contains("edit"))
-        {    let li=e.target.parentElement;
+        {   editFlag=true;
+            let li=e.target.parentElement;
             let str=li.textContent;
-            let str1=str.toString().substring(0,4);
-            let obj= JSON.parse(localStorage.getItem(str1));
-            localStorage.removeItem(Number(str1));
-            list.removeChild(li);
-            document.getElementById('amount').value=obj.amount;
-            document.getElementById('desc').value=obj.desc;
-            document.getElementById('category').value=obj.category;
+            let str1=str.slice(-34);
+            let str2=str1.substring(0,24);
             
+
+            axios.get("https://crudcrud.com/api/a67dbf06587f4775bda035004815effe/ExpenseTracker/"+str2)
+            .then((res)=>{
+                document.getElementById('amount').value=res.data.amount;
+                document.getElementById('desc').value=res.data.desc;
+                document.getElementById('category').value=res.data.category;    
+               
+            })
+            .catch((err)=>{console.log(err);});
+           
+            list.removeChild(li);
+            edUrl=str2;    
         }
     }    
 
